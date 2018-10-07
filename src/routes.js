3afe9af966bab064d9b0e33400e23ffeed3cf6a7
Alexzanderk <kotsarev.a@gmail.com>
2018-10-07 21:53:15 +0300

    create privat & public routes

--- src/routes.js
+++ src/routes.js
@@ -7,21 +7,26 @@ import NewsArticle from './components/Articles/News/Post/index';
 import VideoArticle from './components/Articles/Videos/Video/index';
 import NewsMain from './components/Articles/News/Main/index';
 import VideosMain from './components/Articles/Videos/Main/index';
+import SingIn from './components/SingIn/SingIn';
+import Dashboard from './components/Dashboard/Dashboard';
 
-class Routes extends Component {
-    render() {
-        return (
-            <Layout>
-                <Switch>
-                    <Route path="/" exact component={Home} />
-                    <Route path="/news" exact component={NewsMain} />
-                    <Route path="/videos" exact component={VideosMain} />
-                    <Route path="/articles/:id" exact component={NewsArticle} />
-                    <Route path="/videos/:id" exact component={VideoArticle} />
-                </Switch>
-            </Layout>
-        );
-    }
-}
+import PrivatRoutes from './components/authRoutes/PrivatRoutes';
+import PublicRoutes  from './components/authRoutes/PublicRoutes'
+
+const Routes = props => {
+    return (
+        <Layout user={props.user}>
+            <Switch>
+                <PublicRoutes {...props} restricted={false} path="/" exact component={Home} />
+                <PublicRoutes {...props} restricted={false} path="/news" exact component={NewsMain} />
+                <PublicRoutes {...props} restricted={false} path="/videos" exact component={VideosMain} />
+                <PublicRoutes {...props} restricted={false} path="/articles/:id" exact component={NewsArticle} />
+                <PublicRoutes {...props} restricted={false} path="/videos/:id" exact component={VideoArticle} />
+                <PublicRoutes {...props} restricted={true} path="/sign-in" exact component={SingIn} />
+                <PrivatRoutes {...props} path="/dashboard" exact component={Dashboard} />
+            </Switch>
+        </Layout>
+    );
+};
 
 export default Routes;
